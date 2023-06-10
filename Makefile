388a98b888f73934f2c0c9cbe8ae7547d716cb42
Kunniii <nguyenairport56@gmail.com>
2023-06-10 23:21:40 +0700

    added Docker

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 up:
-	@gunicorn wsgi:app
+	@gunicorn --bind 0.0.0.0:80 wsgi:app
 
 dev:
 	@gunicorn --reload wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
 from app import app
-from dotenv import load_dotenv
-from os import getenv
-
-load_dotenv()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
